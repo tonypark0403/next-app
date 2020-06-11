1ea5049907202696281bd71031a8539fdab54894
Tony Park <tony.park2@ontario.ca>
2020-06-11 00:31:04 -0400

    customized button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,20 @@
-import Head from "next/head";
-import { Button } from "@material-ui/core";
+import Head from 'next/head';
+import { Button } from '@material-ui/core';
+import styled from 'styled-components';
+
+const StyledButton = styled(({ children, colorOptions, ...props }) => (
+  <Button {...props}>{children}</Button>
+))`
+  border: 1px solid #eeeeee;
+  padding: 10px 15px;
+  background-color: ${(props) =>
+    props.colorOptions ? props.colorOptions : 'blue'};
+`;
 
 export default function Home() {
   return (
-    <div className="container">
-      <Button>Test</Button>
+    <div className='container'>
+      <StyledButton>Test</StyledButton>
     </div>
   );
 }
