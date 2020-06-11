5f5424e73d4a552ecb6d1097c2d3c080f98bd7fa
Tony Park <tony.park2@ontario.ca>
2020-06-11 00:24:54 -0400

    applied global and base material ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
+import { StylesProvider, ThemeProvider } from '@material-ui/core';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '@resources/styles/theme';
+import { GlobalStyle, theme } from '@resources/styles';
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
@@ -17,19 +15,12 @@ export default function MyApp(props) {
   }, []);
 
   return (
-    <React.Fragment>
-      <Head>
-        <title>My page</title>
-        <meta
-          name='viewport'
-          content='minimum-scale=1, initial-scale=1, width=device-width'
-        />
-      </Head>
+    <StylesProvider injectFirst>
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
-    </React.Fragment>
+    </StylesProvider>
   );
 }
